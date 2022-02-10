9060e6092510f77f223488cf4f6a625d14acb77c
Gary Shetye <garyshetye@Garys-MacBook-Pro.local>
2022-02-10 13:24:34 -0500

    Done up to 6

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*
 
 In-class activity 07 starter code
-Prof. Mosca 
-Modified: 12/06/21 
+Prof. Mosca
+Modified: 12/06/21
 
 */
 
@@ -10,5 +10,9 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+  let randNum = Math.random()*100;
+
+  let buttonDiv = document.getElementById("random_num");
+  buttonDiv.innerHTML = randNum;
+  console.log("Button 1 was clicked");
 }
